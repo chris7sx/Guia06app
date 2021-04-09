1cc924336d52eaf95fd3a8ec08f7394a041d3909
chris7sx <christianjflores7@gmail.com>
2021-04-09 15:26:18 -0600

    guia06app

---
+++ app/src/main/java/sv/lf171258/guia06app/MainActivity.java
@@ -0,0 +1,15 @@
+package sv.lf171258.guia06app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_relative_layout);
+    }
+}
